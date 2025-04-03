3e146aed2c3811c2518207cb60b27525c2178077
Ayushkr7380 <ayushkumar19999@gmail.com>
2025-04-03 22:52:16 +0530

    pattern code added

---
+++ Takeyouforward/Pattern/pattern.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int rows,cols;
+    cout<<"Enter rows : ";
+    cin>>rows;
+    cout<<"Enter cols : ";
+    cin>>cols;
+    for(int i=0;i<rows;i++){
+        for(int j = 0;j<cols;j++){
+            cout<<"*";
+        }
+        cout<<""<<endl;
+    }
+    return 0;
+}
