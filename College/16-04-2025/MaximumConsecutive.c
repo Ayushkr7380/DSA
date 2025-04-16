e9e6c28086ab651fe114864575b507f2e8fa19ee
Ayushkr7380 <ayushkumar19999@gmail.com>
2025-04-16 16:39:03 +0530

    dsa college

---
+++ College/16-04-2025/MaximumConsecutive.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int arr[] = {1,1,0,1,1,1,1,1,1};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    int countOne = 0;
+    for(int i=0;i<size;i++){
+        if(arr[i] == 1){
+            countOne++;
+        }
+        else if(arr[i] == 0){
+            countOne = 0;
+        }
+    }
+
+    printf("Count : %d",countOne);
+    return 0;
+}
