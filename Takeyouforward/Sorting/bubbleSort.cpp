b2824f0bf3c454eec8f1522dbb9d54c9505c2018
Ayushkr7380 <ayushkumar19999@gmail.com>
2025-04-19 23:36:36 +0530

    bubble sort learning c++

--- Takeyouforward/Sorting/bubbleSort.cpp
+++ Takeyouforward/Sorting/bubbleSort.cpp
@@ -2,14 +2,31 @@
 using namespace std;
 
 void bubbleSort(int arr[],int size){
+    //In this time complexity would be O(n^2). --> Worst Case and Average Case.
+    
+    // for(int i=size-1;i>=0;i--){
+    //     for(int j=0;j<=i-1;j++){
+    //         if(arr[j] > arr[j+1]){
+    //             int temp = arr[j+1];
+    //             arr[j+1] = arr[j];
+    //             arr[j] = temp;
+    //         }
+    //     }
+    // }
+
+    //In this time complexity would be O(n). --> Best Case
     for(int i=size-1;i>=0;i--){
+        int didSwap = 0;
         for(int j=0;j<=i-1;j++){
             if(arr[j] > arr[j+1]){
                 int temp = arr[j+1];
                 arr[j+1] = arr[j];
                 arr[j] = temp;
+
+                didSwap = 1;
             }
         }
+        if(didSwap == 0) break;
     }
 }
 int main(){
