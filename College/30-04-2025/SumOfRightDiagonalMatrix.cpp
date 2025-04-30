ed20918b50f8db7ebcc23f69ab5b14d284ffeafc
Ayush Kumar <142900599+Ayushkr7380@users.noreply.github.com>
2025-04-30 09:36:27 +0000

    dsa practice college

---
+++ College/30-04-2025/SumOfRightDiagonalMatrix.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(){
+    int arr[3][3]={{1,2,3},{4,5,6},{7,8,9}};
+    
+    int sum = 0;
+    
+
+    for(int i=0;i<3;i++){
+        for(int j=2-i;j>=0;j--){
+            sum += arr[i][j];
+        }
+        
+    }
+    
+    printf("Sum of right diagonal element of 3x3 matrix is %d\n",sum);
+
+    return 0;
+}
