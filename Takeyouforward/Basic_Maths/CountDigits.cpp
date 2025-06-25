3f6ef269053cf9fa0894840e205c5d86b0d816e0
ayush kumar <ayushkumar19999@gmail.com>
2025-06-25 23:32:58 +0530

    commnet added

--- Takeyouforward/Basic_Maths/CountDigits.cpp
+++ Takeyouforward/Basic_Maths/CountDigits.cpp
@@ -1,15 +1,22 @@
-#include<bits/stdc++.h>
+#include<bits/stdc++.h>  // Includes all standard C++ libraries
 using namespace std;
+
+// Function to count the number of digits in a given number
 void countDigits(int num){
-    int count = 0;
-    while(num!=0){
-        count++;
-        num/=10;
+    int count = 0;  // Initialize digit count to 0
+
+    // Loop runs until all digits are removed
+    while(num != 0){
+        count++;     // Increment count for each digit
+        num /= 10;   // Remove the last digit (integer division by 10)
     }
-    cout<<"Count is : "<<count<<endl;
+
+    // Output the total count of digits
+    cout << "Count is : " << count << endl;
 }
+
 int main(){
-    int num = 12121;
-    countDigits(num);
-    return 0;
+    int num = 12121;         // Input number
+    countDigits(num);        // Call function to count digits
+    return 0;                // Indicate suc
 }
