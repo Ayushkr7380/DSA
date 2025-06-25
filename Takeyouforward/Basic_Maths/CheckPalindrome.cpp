d27c9b1121a8588202b40072bf6595be8d5cf54f
ayush kumar <ayushkumar19999@gmail.com>
2025-06-25 23:34:08 +0530

    commnet added

--- Takeyouforward/Basic_Maths/CheckPalindrome.cpp
+++ Takeyouforward/Basic_Maths/CheckPalindrome.cpp
@@ -1,23 +1,28 @@
-#include<bits/stdc++.h>
+#include<bits/stdc++.h>  // Includes all standard C++ libraries
 using namespace std;
 
+// Function to check whether a number is a palindrome
 void checkPalindrome(int num){
-    int rev = 0,temp=num;
-    while(temp!=0){
-        rev = rev*10 + (temp%10);
-        temp/=10;
+    int rev = 0;        // To store the reversed number
+    int temp = num;     // Store original number for comparison
+
+    // Reverse the number
+    while(temp != 0){
+        rev = rev * 10 + (temp % 10);  // Add last digit to rev
+        temp /= 10;                    // Remove the last digit
     }
+
+    // Check if the reversed number is equal to the original
     if(rev == num){
-        cout<<num<<" is a palindrome number."<<endl;
+        cout << num << " is a palindrome number." << endl;
     }
     else{
-        cout<<num<<" is not a palindrome number."<<endl;
+        cout << num << " is not a palindrome number." << endl;
     }
 }
-int main(){
-    
-    int num = 151;
-    checkPalindrome(num);
 
-    return 0;
+int main(){
+    int num = 151;             // Input number
+    checkPalindrome(num);      // Check if it's a palindrome
+    return 0;                  // Indicate successful termination
 }
