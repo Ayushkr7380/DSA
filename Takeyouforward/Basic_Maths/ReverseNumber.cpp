6aac37435bbe243e3a7232dced84a9827cb641fc
ayush kumar <ayushkumar19999@gmail.com>
2025-06-26 23:25:22 +0530

    comment added

--- Takeyouforward/Basic_Maths/ReverseNumber.cpp
+++ Takeyouforward/Basic_Maths/ReverseNumber.cpp
@@ -1,18 +1,23 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-void reverseNum(int num){
-    int rev = 0,temp=num;
-    while(temp!=0){
-        rev = rev*10 + (temp%10);
-        temp/=10;
-    }
-    cout<<"Reverse of "<<num<<" is "<<rev<<endl;
-}
-int main(){
-    
-    int num = 12342;
-    reverseNum(num);
+// Function to reverse the digits of a number
+void reverseNum(int num) {
+    int rev = 0, temp = num;
 
-    return 0;
+    // Loop to reverse the number
+    while (temp != 0) {
+        rev = rev * 10 + (temp % 10); // Add last digit of temp to rev
+        temp /= 10;                   // Remove the last digit from temp
+    }
+
+    // Output the reversed number
+    cout << "Reverse of " << num << " is " << rev << endl;
+}
+
+int main() {
+    int num = 12342; // Input number
+    reverseNum(num); // Call function to reverse the number
+
+    return 0; // Exit program
 }
