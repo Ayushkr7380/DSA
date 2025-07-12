aa04fb0b5497c5e0e16cbf923d3658c7af2af036
ayush kumar <ayushkumar19999@gmail.com>
2025-07-13 01:03:21 +0530

    queue using array

---
+++ College/2nd sem MCA Exam/queueUsingArray.c
@@ -0,0 +1,66 @@
+#include<stdio.h>
+#define size 5
+
+int queue[size];
+
+int front  = -1;
+int rear = -1;
+void enqueue(int n){
+
+    if(rear == size -1){
+        printf("\n Queue is full.\n");
+        return;
+    }
+    if(front = -1 && rear == -1){
+        front = 0;
+        rear = 0;
+
+        queue[rear] = n;
+
+    }
+    
+    else{
+        rear++;
+        queue[rear] = n;
+    }
+
+    printf("\n %d is added in the queue.",n);
+}
+void dequeue(){
+    if(front == -1){
+        printf("\nQueue is empty.\n");
+        return;
+    }
+
+    printf("\nDeleted element: %d\n", queue[front]);
+
+    if(front == rear){
+        front = -1;
+        rear = -1;
+    }else{
+
+        front++;
+    }
+}
+
+void display(){
+    printf(" \n -- DISPLAY -- \n");
+    for(int i=front;i<= rear ;i++){
+        printf("%d \t",queue[i]);
+    }
+}
+int main(){
+    enqueue(10);
+    enqueue(20);
+    enqueue(30);
+    enqueue(40);
+
+    display();
+
+    dequeue();
+
+    display();
+
+
+    return 0;
+}
